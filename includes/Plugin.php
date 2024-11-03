bfe434b43c8cf63522910833a832b33069909f09
Arūnas Liuiza <ask@arunas.co>
2024-11-03 12:51:59 +0200

    Refactor options

--- includes/Plugin.php
+++ includes/Plugin.php
@@ -18,18 +18,13 @@ class Plugin {
 
     private function __construct() {}
 
-    protected $options = [
-        'password' => 'G483 3CLL BlXt qpjc 4r5y Txcc',
-        'require_auth' => true,
-        'hostnames' => [
-            'api.wordpress.org' => 'localhost:10169/wp-json/lifejacket/v1/api',
-            'downloads.wordpress.org' => 'localhost:10169/wp-json/lifejacket/v1/downloads',    
-        ],
-    ];
+    protected $options;
 
     public function init() {
         add_filter( 'http_allowed_safe_ports', [ $this, 'maybe_allow_port' ] );
         add_filter( 'pre_http_request', [ $this, 'maybe_proxy_dotorg' ], 1, 3 );
+
+        $this->options = new Options();
     }
 
     public function maybe_allow_port( $ports) {
@@ -38,10 +33,20 @@ class Plugin {
         return $ports;
     }
 
+    public function get_hostnames() {
+        $server = $this->options->get( 'server' );
+        $hostnames = [
+            'api.wordpress.org' => $server . '/api',
+            'downloads.wordpress.org' => $server . '/downloads',    
+        ];
+        $hostnames = apply_filters( 'lifejacket/hostnames', $hostnames );
+        return $hostnames;
+    }
+
     public function maybe_proxy_dotorg( $response, $parsed_args, $url ) {
         $domain = '';
         $endpoint = '';
-        foreach ( $this->options['hostnames'] as $hostname => $replacement ) {
+        foreach ( $this->get_hostnames() as $hostname => $replacement ) {
             if ( 1 === preg_match( '|^https?://' . $hostname . '|ims', $url ) ) {
                 $domain = $hostname;
                 $endpoint = $replacement;
@@ -50,11 +55,11 @@ class Plugin {
         }
 
         if ( $domain && $endpoint ) {
-            if ( $this->options['require_auth'] ) {
+            if ( $this->options->get( 'require_auth' ) ) {
                 $parsed_args['headers'] = $parsed_args['headers'] ?? [];
-                $parsed_args['headers']['X-WP-Application-Password'] = $this->options['password'];    
+                $parsed_args['headers']['X-WP-Application-Password'] = $this->options->get( 'application_password' );    
             }
-            $url = preg_replace( '|^https?://' . $domain . '|ims', 'http://' . $endpoint, $url );
+            $url = preg_replace( '|^https?://' . $domain . '|ims', $endpoint, $url );
             $response = wp_remote_request( $url, $parsed_args );
         }
         
