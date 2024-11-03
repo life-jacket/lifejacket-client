46df652d2a78f4afb45d47218163378c42ca845c
Arūnas Liuiza <ask@arunas.co>
2024-11-03 17:09:51 +0200

    Update plugin header

--- lifejacket-client.php
+++ lifejacket-client.php
@@ -2,6 +2,12 @@
 /**
  * Plugin Name: LifeJacket Client
  * Version: 0.1.0
+ * Plugin URI: https://github.com/life-jacket/lifejacket-client
+ * Description: Enables you to use a custom update server for your WordPress site. Part of LifeJacket project
+ * Author: LifeJacket
+ * Author URI: https://github.com/life-jacket
+ * License: GPLv2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
 require_once __DIR__ . '/vendor/autoload.php';
